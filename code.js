3959a8360beadff0b93391cb374796243b318aff
Sacagawea <88001511+NormalCarStuff@users.noreply.github.com>
2021-07-28 12:32:24 -0700

    Add files via upload

---
+++ code.js
@@ -0,0 +1,20 @@
+function goForward(){
+    console.log("Go Forward");
+}
+
+function goBackward(){
+    console.log("Go Backward");
+}
+
+function goLeft(){
+    console.log("Go Left");
+}
+
+function goRight(){
+    console.log("Go Right");
+}
+
+function allStop(){
+    console.log("Stop All");
+}
+
